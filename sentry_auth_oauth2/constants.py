acdf368c2737dfe4145111439fdc53aa67483b19
fliespl <a.rzadkowolski@gmail.com>
2021-11-19 21:09:00 +0100

    Update constants.py

--- sentry_auth_oauth2/constants.py
+++ sentry_auth_oauth2/constants.py
@@ -20,6 +20,8 @@ ERR_NO_SINGLE_VERIFIED_PRIMARY_EMAIL = 'We were unable to find a single verified
 
 SCOPE = getattr(settings, 'OAUTH2_SCOPE', 'user:email')
 
+BASE_DOMAIN = getattr(settings, 'OAUTH2_BASE_DOMAIN', 'oauth2.com')
+
 ACCESS_TOKEN_URL = 'https://{0}/oauth/v2/token'.format(BASE_DOMAIN)
 
 AUTHORIZE_URL = 'https://{0}/oauth/v2/auth'.format(BASE_DOMAIN)
