10c49f99f7189d09cc0dc681efd5e40625783aae
fliespl <a.rzadkowolski@gmail.com>
2021-11-19 21:15:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ tests_require = [
 
 setup(
     name='sentry-auth-oauth2',
-    version='0.2.0',
+    version='0.2.1',
     author='Sentry',
     author_email='support@getsentry.com',
     url='https://www.getsentry.com',
