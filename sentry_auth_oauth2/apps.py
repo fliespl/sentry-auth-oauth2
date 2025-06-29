53125c2f23d041cac8a63388bfb9cec5ed838989
fliespl <a.rzadkowolski@gmail.com>
2025-06-29 21:47:46 +0200

    Update apps.py

--- sentry_auth_oauth2/apps.py
+++ sentry_auth_oauth2/apps.py
@@ -4,8 +4,9 @@ from django.apps import AppConfig
 class Config(AppConfig):
     name = "sentry_auth_oauth2"
 
-    def ready(self):
+    def ready(self) -> None:
         from sentry import auth
+        
         from .provider import GenericOAuth2Provider
 
         auth.register(GenericOAuth2Provider)
