bcb46d9a7b6ec652875c7a4d1a6e22dac16f625d
fliespl <a.rzadkowolski@gmail.com>
2025-06-29 22:06:42 +0200

    Update views.py

--- sentry_auth_oauth2/views.py
+++ sentry_auth_oauth2/views.py
@@ -1,4 +1,5 @@
-from django import forms
+from __future__ import annotations
+
 from sentry.auth.view import AuthView
 from sentry.models import AuthIdentity
 
@@ -9,25 +10,25 @@ def _get_name_from_email(email):
     """
     Given an email return a capitalized name. Ex. john.smith@example.com would return John Smith.
     """
-    name = email.rsplit('@', 1)[0]
-    name = ' '.join([n_part.capitalize() for n_part in name.split('.')])
+    name = email.rsplit("@", 1)[0]
+    name = " ".join(n_part.capitalize() for n_part in name.split("."))
     return name
 
 
 class FetchUser(AuthView):
-    def __init__(self, client_id, client_secret, org=None, *args, **kwargs):
+    def __init__(self, client_id, client_secret, org=None, *args, **kwargs) -> None:
         self.org = org
         self.client = GenericClient(client_id, client_secret)
-        super(FetchUser, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 
-    def handle(self, request, helper):
-        access_token = helper.fetch_state('data')['access_token']
+    def handle(self, request: HttpRequest, pipeline):
+        access_token = pipeline.fetch_state('data')['access_token']
 
         user = self.client.get_user(access_token)
 
         if not user.get('name'):
             user['name'] = _get_name_from_email(user['email'])
 
-        helper.bind_state('user', user)
+        pipeline.bind_state('user', user)
 
-        return helper.next_step()
+        return pipeline.next_step()
