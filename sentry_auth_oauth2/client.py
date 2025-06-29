6d352b4cec7308bc2e945419161baeba6ea2db08
fliespl <a.rzadkowolski@gmail.com>
2025-06-29 21:54:35 +0200

    Update client.py

--- sentry_auth_oauth2/client.py
+++ sentry_auth_oauth2/client.py
@@ -1,21 +1,31 @@
+from __future__ import annotations
+
+import orjson
+
 from requests.exceptions import RequestException
+
 from sentry import http
-from sentry.utils import json
 
 from .constants import API_DOMAIN
 
 
 class GenericApiError(Exception):
     def __init__(self, message='', status=0):
-        super(GenericApiError, self).__init__(message)
+        super().__init__(message)
         self.status = status
 
 
-class GenericClient(object):
+class GenericClient:
     def __init__(self, client_id, client_secret):
         self.client_id = client_id
         self.client_secret = client_secret
         self.http = http.build_session()
+
+    def __enter__(self) -> GenericClient:
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        self.http.close()
 
     def _request(self, path, access_token):
         params = {
@@ -33,10 +43,10 @@ class GenericClient(object):
                 headers=headers,
             )
         except RequestException as e:
-            raise GenericApiError(unicode(e), status=getattr(e, 'status_code', 0))
+            raise GenericApiError(f"{e}", status=getattr(e, 'status_code', 0))
         if req.status_code < 200 or req.status_code >= 300:
             raise GenericApiError(req.content, status=req.status_code)
-        return json.loads(req.content)
+        return orjson.loads(req.content)
 
     def get_user(self, access_token):
         return self._request('/user/me', access_token)
