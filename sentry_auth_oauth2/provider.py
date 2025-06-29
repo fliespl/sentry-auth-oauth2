029800fc3995971dd0f9c678e7876e08f366b03a
fliespl <a.rzadkowolski@gmail.com>
2025-06-29 23:12:48 +0200

    Update provider.py

--- sentry_auth_oauth2/provider.py
+++ sentry_auth_oauth2/provider.py
@@ -13,7 +13,7 @@ class GenericOAuth2Provider(OAuth2Provider):
     access_token_url = ACCESS_TOKEN_URL
     authorize_url = AUTHORIZE_URL
     name = 'OWL'
-    key = 'owl'
+    key = 'oauth2'
 
     def get_client_id(self):
         return CLIENT_ID
